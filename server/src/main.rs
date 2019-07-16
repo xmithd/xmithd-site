7e66fa2c7ecada32026d5c0567527b30afd58bbc
xmithd <xmithd@users.noreply.github.com>
2019-07-16 17:42:00 -0400

    Make SPA available on a scpecific /webapp scope allowing the main site to have static files

--- server/src/main.rs
+++ server/src/main.rs
@@ -8,9 +8,21 @@ use actix_web::{
   Result,
 };
 
-fn index() -> Result<fs::NamedFile> {
-  let file = fs::NamedFile::open("../ui/build/index.html")?;
+fn render_file(file_name: &str) -> Result<fs::NamedFile> {
+  let file = fs::NamedFile::open(file_name)?;
   Ok(file)
+}
+
+fn ui_app() -> Result<fs::NamedFile> {
+  render_file("../ui/build/index.html")
+}
+
+fn home_page() -> Result<fs::NamedFile> {
+  render_file("./templates/home.html")
+}
+
+fn favicon() -> Result<fs::NamedFile> {
+  render_file("./templates/favicon.ico")
 }
 
 fn main() -> io::Result<()> {
@@ -20,8 +32,10 @@ fn main() -> io::Result<()> {
 
   HttpServer::new(|| {
     App::new()
-    .service(web::resource("/").route(web::get().to(index)))
-    .service(fs::Files::new("/", "../ui/build"))
+    .service(web::resource("/").route(web::get().to(home_page)))
+    .service(web::resource("/favicon.ico").route(web::get().to(favicon)))
+    .service(web::resource("/webapp").route(web::get().to(ui_app)))
+    .service(fs::Files::new("/webapp", "../ui/build"))
   })
   .bind(&addr)?
   .run()
