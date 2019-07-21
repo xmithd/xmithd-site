be18dd6359c523a69ef996cf5406492b627f257a
xmithd <xmithd@users.noreply.github.com>
2019-07-21 12:15:42 -0400

    Separate config into its own module (constants for now)

---
+++ server/src/config.rs
@@ -0,0 +1,5 @@
+
+// Some config constants
+
+pub const PORT : u32 = 3001;
+pub const REALTIME_CHAT_APP_ROOT: &str = "../ui/build";
--- server/src/main.rs
+++ server/src/main.rs
@@ -1,5 +1,7 @@
 extern crate actix_web;
 
+mod config;
+
 use std::{io};
 
 use actix_files as fs;
@@ -7,14 +9,16 @@ use actix_web::{
   web, App, HttpServer,
   Result,
 };
+use actix_web::middleware::Logger;
 
 fn render_file(file_name: &str) -> Result<fs::NamedFile> {
   let file = fs::NamedFile::open(file_name)?;
   Ok(file)
 }
 
-fn ui_app() -> Result<fs::NamedFile> {
-  render_file("../ui/build/index.html")
+fn ui_app(webapp_root: &str) -> Result<fs::NamedFile> {
+  let index_path= format!("{}/index.html", webapp_root);
+  render_file(&index_path)
 }
 
 fn home_page() -> Result<fs::NamedFile> {
@@ -26,16 +30,17 @@ fn favicon() -> Result<fs::NamedFile> {
 }
 
 fn main() -> io::Result<()> {
-  let port: u32 = 3001;
-  let addr = format!("127.0.0.1:{}", port);
+  let addr = format!("127.0.0.1:{}", config::PORT);
   println!("Starting http server at http://{}", &addr);
 
-  HttpServer::new(|| {
+
+  HttpServer::new( || {
     App::new()
+        .wrap(Logger::new("%a %t \"%r\" %s %b \"%{Referer}i\" \"%{User-Agent}i\" %T"))
     .service(web::resource("/").route(web::get().to(home_page)))
     .service(web::resource("/favicon.ico").route(web::get().to(favicon)))
-    .service(web::resource("/webapp").route(web::get().to(ui_app)))
-    .service(fs::Files::new("/webapp", "../ui/build"))
+    .service(web::resource("/chatapp").route(web::get().to(|| ui_app(config::REALTIME_CHAT_APP_ROOT))))
+    .service(fs::Files::new("/chatapp", config::REALTIME_CHAT_APP_ROOT))
   })
   .bind(&addr)?
   .run()
